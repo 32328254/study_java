f042c55f5a3a42dbe85accabe17645f44259eeb2
liu <32328254@qq.com>
2017-05-03 00:45:29 +0800

    hello

--- day02/src/DataTyoeDemo.java
+++ day02/src/DataTyoeDemo.java
@@ -3,11 +3,13 @@
  */
 public class DataTyoeDemo {
     public static void main(String[] args){
-        double a = 3.0;
-        double b = 2.9;
-        System.out.println(a-b);
-        double d = 6.0;
-        double e = 4.9;
-        System.out.println(d-e);
+//        double a = 3.0;
+//        double b = 2.9;
+//        System.out.println(a-b);
+//        double d = 6.0;
+//        double e = 4.9;
+//        System.out.println(d-e);
+        int a = 250;
+        //int b = 10000000000;
     }
 }
