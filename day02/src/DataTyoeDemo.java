167e9fc8f3b00fc8a0b472f8bee775b4bcd30c0d
“liuhaoran” <liuhaoran1@jd.com>
2017-05-03 21:10:01 +0800

    commit study java

--- day02/src/DataTyoeDemo.java
+++ day02/src/DataTyoeDemo.java
@@ -9,7 +9,30 @@ public class DataTyoeDemo {
 //        double d = 6.0;
 //        double e = 4.9;
 //        System.out.println(d-e);
-        int a = 250;
-        //int b = 10000000000;
+//        int a = 250;
+//        System.out.println("5/2="+5/2);
+//        System.out.println("2/5="+2/5);
+//        //int b = 10000000000;
+//        int c = 2147483647;
+//        c=c+1;
+//        System.out.println(c);
+//        //Long d = 10000000000;
+//        Long e = 10000000000L;
+//        Long f = 1000000000*2*10L;
+//        System.out.println(f);
+//        Long g = 1000000000*3*10L;
+//        System.out.println(g);
+//        Long h = 1000000000L*3*10;
+//        System.out.println(h);
+//        Long i = System.currentTimeMillis();
+//        System.out.println(i);
+//        //float j = 3.14;
+//        float k,l;
+//        k= (float) 6.0;
+//        l= (float) 4.9;
+//        System.out.println(k-l);
+        boolean b1 = true;
+        boolean b2 = false;
+
     }
 }
