426d54f224f783aa186544732547f2178f727287
“liuhaoran” <liuhaoran1@jd.com>
2017-05-03 14:05:19 +0800

    commit study java

---
+++ day02/src/ForNumber.java
@@ -0,0 +1,10 @@
+/**
+ * Created by liuhaoran1 on 2017/5/3.
+ */
+public class ForNumber {
+    public static void main(String[] args){
+        for (int i=4;i<=48;i++){
+            System.out.print("f"+i+" ");
+        }
+    }
+}
